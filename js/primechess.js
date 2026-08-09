407f2c612ed4cc2be91f352b5d9bfe565fbfddc6
Michael Darrow <michaelnonagon@gmail.com>
2026-08-08 19:37:23 -0500

    added another prime, and some settings

--- js/primechess.js
+++ js/primechess.js
@@ -2,13 +2,38 @@
 // Game, Board, Tile, Piece, StatusEffect, Collide
 //   Selected, Narrative, Lore, MoveSelected
 //   Local Multi / AI switch 
-
+function toShuffled(array) {
+  let K = array.toSpliced();
+  for (let i = 0; i < K.length; i++) {
+    let j = i+Math.floor(Math.random()*(K.length-i)); // How pleasing, no off-by-one errors!
+    [K[i], K[j]] = [K[j], K[i]];
+  }
+  return K;
+}
 let canvas, context;
-let currentTurn = 1;
-let boardPieces = [[-2,-1,0,0,0,0,1,2],[-3,-1,0,0,0,0,1,3],[-5,-1,0,0,0,0,1,5],[-7,-1,0,0,0,0,1,7],[-11,-1,0,0,0,0,1,11],[-5,-1,0,0,0,0,1,5],[-3,-1,0,0,0,0,1,3],[-2,-1,0,0,0,0,1,2]];
-let boardStates = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
-let selectedLocation = [];
+let currentTurn,boardPieces,boardStates,selectedLocation,primes;
 let computer = false;
+let chaosmode = false;
+function resetGame() {
+ currentTurn = 1;
+ boardPieces = [[-2,-1,0,0,0,0,1,2],[-3,-1,0,0,0,0,1,3],[-5,-1,0,0,0,0,1,5],[-7,-1,0,0,0,0,1,7],[-11,-1,0,0,0,0,1,11],[-5,-1,0,0,0,0,1,5],[-3,-1,0,0,0,0,1,3],[-2,-1,0,0,0,0,1,2]];
+ boardStates = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+ selectedLocation = [];
+ let P = [2,3,5,7,11,13];
+ if (chaosmode) {
+  P = toShuffled(P);
+ }
+ primes = {
+  yellower: P[0],
+  mover: P[1],
+  uncable: P[2], // uncapturable
+  cloner: P[3],
+  increment: P[4],
+  rounder: P[5]
+ }
+ drawBoard();
+}
+
 // i'm thinking negative numbers represent the opponent's pieces and positive numbers represent your pieces
 const boardDark = new Image();
 boardDark.src = 'img/tiledk.png';
@@ -24,8 +49,8 @@ function isValidMove(starti, startj, endi, endj) {
       if (boardStates[endi][endj] > 0) {
         return false;
       } else {
-        if (Math.abs(starti - endi) + Math.abs(startj - endj) < 2 + discreteLog(boardPieces[starti][startj], 3)) {
-          if ((boardPieces[endi][endj] == 0) || (discreteLog(boardPieces[starti][startj], 5) >= discreteLog(boardPieces[endi][endj], 5))) {
+        if (Math.abs(starti - endi) + Math.abs(startj - endj) < 2 + discreteLog(boardPieces[starti][startj], primes.mover)) {
+          if ((boardPieces[endi][endj] == 0) || (discreteLog(boardPieces[starti][startj], primes.uncable) >= discreteLog(boardPieces[endi][endj], primes.uncable))) {
             return true;
           } else {
             return false;
@@ -38,6 +63,17 @@ function isValidMove(starti, startj, endi, endj) {
   } else {
     return false;
   }
+}
+
+function showSettings() {
+  document.getElementById("settingsDialog").showModal();
+}
+
+function saveSettings() {
+  computer = document.getElementById("computerPlays").checked;
+  chaosmode = document.getElementById("chaosMode").checked;
+  resetGame();
+  document.getElementById("settingsDialog").close();
 }
 
 function isValidSelection(i, j) {
@@ -83,10 +119,11 @@ function beforemovesteps() {
 function makeMove(starti, startj, endi, endj) {
   let startNumber = boardPieces[starti][startj]; 
   let endNumber = boardPieces[endi][endj];
-  boardStates[starti][startj] = 2 * discreteLog(startNumber, 2);
+  boardStates[starti][startj] = 2 * discreteLog(startNumber, primes.yellower);
   boardPieces[endi][endj] = startNumber - boardPieces[endi][endj];
-  boardPieces[starti][startj] = -endNumber * discreteLog(startNumber, 7);
-  boardPieces[endi][endj] += discreteLog(startNumber, 11) * Math.sign(startNumber);
+  boardPieces[starti][startj] = -endNumber * discreteLog(startNumber, primes.cloner);
+  boardPieces[endi][endj] += discreteLog(startNumber, primes.increment) * Math.sign(startNumber);
+  boardPieces[endi][endj] = Math.ceil(Math.abs(boardPieces[endi][endj]) / (primes.rounder ** discreteLog(startNumber, primes.rounder))) * (primes.rounder ** discreteLog(startNumber, primes.rounder)) * Math.sign(startNumber);
   currentTurn = currentTurn * -1;
   beforemovesteps();
   if (findallValidMoves().length == 0) {declareLoss();}
@@ -126,16 +163,18 @@ function getDescription(number) {
 
 function getPrimeDescription(number, prime, power) {
   switch (prime) {
-    case 2:
+    case primes.yellower:
       return 'Turns the square it left unusable for ' + power + " of your opponent's turns. ";
-    case 11:
+    case primes.increment:
       return 'Increases by ' + power + ' after moving. ';
-    case 7:
+    case primes.cloner:
       return 'Leaves behind a clone of any piece it captures. ';
-    case 3:
+    case primes.mover:
       return 'Can move up to ' + power + ' additional squares. ';
-    case 5:
-      return 'Cannot be captured by a piece with fewer than ' + power + ' factors of 5. '
+    case primes.uncable:
+      return 'Cannot be captured by a piece with fewer than ' + power + ' factors of ' + primes.uncable + '. '
+    case primes.rounder:
+      return 'Rounds up to the nearest multiple of ' + prime ** power + ' after moving. '
     default:
       if (power == 1) {
         return 'Has a useless factor of ' + prime + '. ';
@@ -236,6 +275,7 @@ function drawBoard() {
   }  
 }
 
+
 document.addEventListener("DOMContentLoaded", function() {
   canvas = document.getElementById("gameBoard");
   context = canvas.getContext('2d');
@@ -263,4 +303,9 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   });
+
+  resetGame();
+
+  document.getElementById("settingsButton").addEventListener('click', showSettings);
+  document.getElementById("unsettingsButton").addEventListener('click', saveSettings);
 });
